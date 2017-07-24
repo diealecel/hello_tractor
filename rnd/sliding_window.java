3df4d12fdf98a7add0ddc5bd2ec2467934312103
diealecel <diealecel@gmail.com>
2017-07-24 16:12:08 -0700

    created initial Java port

--- rnd/sliding_window.java
+++ rnd/sliding_window.java
@@ -1,1 +1,36 @@
 // Wow this is incredible
+import java.util.*;
+import java.io.*;
+
+class instance {
+    private int heading;
+    private int speed;
+
+    instance(int heading, int speed) {
+        this.heading = heading;
+        this.speed = speed;
+    }
+
+    int get_heading() {
+        return heading;
+    }
+
+    int get_speed() {
+        return speed;
+    }
+}
+
+
+
+public class sliding_window {
+    ArrayList<String[]> csv_reader(String filename) {
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+
+        String line = null;
+        
+    }
+
+    public static void main(String[] args) {
+        // Main method.
+    }
+}
